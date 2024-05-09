9e02737bae75c606256e57b20c249371a37afb00
Artem Zaytsev <azrail@MacBook-Pro-Artem.local>
2024-05-09 17:37:16 +0300

    f

--- inititalizing_utils/fd.sh
+++ inititalizing_utils/fd.sh
@@ -1,4 +1,4 @@
-!#/bin/bash/
+#!/bin/bash/
 fileid="14QcsxQILplN8nXSa87ACOOYiCebiVPxM"
 filename="lmdb.zip"
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
